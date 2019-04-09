741d37ae922ca960593103958d092178900767ba
wang <wang@wangdeMacBook-Pro.local>
2019-04-09 15:28:08 +0800

    url

---
+++ FYBase/Classes/Tool/Marco/FYURL.h
@@ -0,0 +1,29 @@
+//
+//  FYURL.h
+//  FYBase
+//
+//  Created by wang on 2019/4/9.
+//  Copyright © 2019 wang. All rights reserved.
+//填写各个网络请求的url地址
+
+#import <Foundation/Foundation.h>
+
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+extern NSString *const HostAPI; //rootApi
+
+//MARK: 首页
+
+//MARK: 我的
+
+
+
+
+@interface FYURL : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FYBase/Classes/Tool/Marco/FYURL.m
@@ -0,0 +1,24 @@
+//
+//  FYURL.m
+//  FYBase
+//
+//  Created by wang on 2019/4/9.
+//  Copyright © 2019 wang. All rights reserved.
+//
+
+#import "FYURL.h"
+#define kInDeBug
+
+#ifdef kInDeBug
+NSString *const HostAPI = @"2";
+#else
+NSString *const HostAPI = @"33";
+#endif
+
+
+
+
+
+@implementation FYURL
+
+@end
